515a121c59dee2154de4033dbb9b184e9199f6ef
Bruce Ediger <bediger8@gmail.com>
2020-06-09 08:39:23 -0600

    item from a list with access only to that item

---
+++ removeitem.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"linked_lists/list"
+)
+
+func main() {
+
+	desired, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Remove list element containing '%c'\n", desired)
+
+	head := list.Compose(os.Args[2:])
+
+	list.Print(head)
+	fmt.Println()
+
+	node := list.FindItem(head, desired)
+
+	switch node {
+	case nil:
+		fmt.Printf("Did not find '%d' in list\n", desired)
+		return
+	default:
+		fmt.Printf("Element at '%p' contains '%d'\n", node, node.Data)
+	}
+
+	list.Remove(node)
+
+	fmt.Printf("list without '%d':\n", desired)
+	list.Print(head)
+	fmt.Println()
+}
+
+func Remove(node *Node) {
+	for node != nil {
+		tmp := node.Next
+		if tmp != nil {
+			node.Data = tmp.Data
+			node.Next = tmp.Next
+		}
+		node = node.Next
+	}
+}
